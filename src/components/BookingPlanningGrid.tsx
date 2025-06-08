4f2d950ec2150c78bd04669e2db91c6dc4b6519d
[dyad] <git@dyad.sh>
2025-06-09 01:30:40 +0200

    Updated src/components/BookingPlanningGrid.tsx

--- src/components/BookingPlanningGrid.tsx
+++ src/components/BookingPlanningGrid.tsx
@@ -25,8 +25,8 @@ interface Property {
 
 // Define your properties here with their actual Krossbooking room IDs
 const properties: Property[] = [
-  { id: '1', name: '2c' },
-  { id: '1', name: 'Plein sud' },
+  { id: '36', name: '2c' },
+  { id: '22', name: 'Plein sud' },
 ];
 
 const BookingPlanningGrid: React.FC = () => {
